78b5d601e743eaf78be8df0d691d56e88bb40775
Your Name <you@example.com>
2019-03-24 10:33:55 +0300

    feat 2

--- src/main/java/com/javarzn/training/spel/MyTestClass.java
+++ src/main/java/com/javarzn/training/spel/MyTestClass.java
@@ -3,11 +3,21 @@ package com.javarzn.training.spel;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class MyTestClass {
     private String myStringProperty;
+    private String adjustedAmount;
+    private String circumference;
+    private String average;
+    private String remainder;
+    private String area;
+    private String fullName;
+    private double piValue;
+
 
 //    public MyTestClass(String myStringProperty) {
 //        this.myStringProperty = myStringProperty;
@@ -15,7 +25,9 @@ public class MyTestClass {
 //    }
 
     public void init() {
+        System.out.println(this.toString());
 //        this.myStringProperty = myStringProperty;
-        System.out.println(this.myStringProperty);
+//        System.out.println(this.myStringProperty);
+//        System.out.println(this.piValue);
     }
 }
